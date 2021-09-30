596526faca66c3b266f78cd58b51c42b9315ee1f
abrorbek <abrorbekgithub@gmail.com>
2021-09-30 22:21:03 +0500

    new function fetch one and testing

--- wikibot/exchange.py
+++ wikibot/exchange.py
@@ -3,8 +3,13 @@
 import requests
 
 api_key = '1e5a955f17-994510bad0-r099up'
+
+
+############fetch-all
+"""
 # res = requests.get(url=f'https://api.fastforex.io/fetch-all?api_key={api_key}')
 # print(res.json())
+
 headers = {"Accept":"applicaton/json"}
 url = f'https://api.fastforex.io/fetch-all?api_key={api_key}'
 
@@ -16,6 +21,22 @@ print(json_res['base'])
 # print(json_res['results'])
 print(json_res['updated'])
 print(json_res['ms'])
+
+"""
+#################fetch one
+
+url = f'https://api.fastforex.io/fetch-one?api_key={api_key}'
+
+querystring = {"from":"USD","to":"UZS"}
+
+headers = {"Accept":"application/json"}
+
+res=requests.request(method='GET',url=url,headers=headers,params=querystring)
+
+resj = res.json()
+print(resj['base'])
+print(resj['result'])
+print(resj['updated'])
 
 
 
